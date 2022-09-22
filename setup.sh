9325d18c2177009c463c0d547b4e93ef5c8e54ae
inoyaksorojawi <71470149+inoyaksorojawi@users.noreply.github.com>
2022-09-22 08:17:56 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -46,9 +46,9 @@ domain=$(cat /etc/xray/domain)
 domain2=$(cat /etc/xray/domain2)
 domain3=$(cat /etc/xray/domain3)
 #if [ -f "/etc/v2ray/domain" ]; then
+if [ -f "/etc/xray/domain2" ];
+if [ -f "/etc/xray/domain3" ];
 if [ -f "/etc/xray/domain" ]; then
-if [ -f "/etc/xray/domain2" ]; then
-if [ -f "/etc/xray/domain3" ]; then
 echo "Script Already Installed"
 exit 0
 fi
