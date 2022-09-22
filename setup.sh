e7f7d794feb2c8590747ab82d0d535b8063c6504
inoyaksorojawi <71470149+inoyaksorojawi@users.noreply.github.com>
2022-09-22 07:30:01 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -42,9 +42,13 @@ wisnuvpnnnnnnnnnn="raw.githubusercontent.com/inoyaksorojawi/large/sae/ohp"
 # Getting
 MYIP=$(wget -qO- ipinfo.io/ip);
 rm -f setup.sh
-
+domain=$(cat /etc/xray/domain)
+domain2=$(cat /etc/xray/domain2)
+domain3=$(cat /etc/xray/domain3)
 #if [ -f "/etc/v2ray/domain" ]; then
 if [ -f "/etc/xray/domain" ]; then
+if [ -f "/etc/xray/domain2" ]; then
+if [ -f "/etc/xray/domain3" ]; then
 echo "Script Already Installed"
 exit 0
 fi
