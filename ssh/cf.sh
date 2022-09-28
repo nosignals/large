84da81c3483d67fafb81b06391fdb96463236bf4
inoyaksorojawi <71470149+inoyaksorojawi@users.noreply.github.com>
2022-09-28 09:23:17 +0700

    Update cf.sh

--- ssh/cf.sh
+++ ssh/cf.sh
@@ -85,8 +85,7 @@ mkdir -p /etc/xray
 cp /root/domain2 /etc/xray
 rm -f /root/cf.sh
 
-pro=$(</dev/urandom tr -dc a-z0-9 | head -c2)
-SUB_DO=$pro.gandring.my.id
+SUB_DO=gg.gandring.my.id
 CF_ID=djarumpentol01@gmail.com
 CF_KEY=00dffab0559c394b8e9dcd7b05fcf1fcd274d
 set -euo pipefail
