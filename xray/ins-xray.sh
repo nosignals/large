f6b54bdf8e22e157cc34ffda8f00f117294785f4
inoyaksorojawi <71470149+inoyaksorojawi@users.noreply.github.com>
2022-10-03 11:44:49 +0700

    Update ins-xray.sh

--- xray/ins-xray.sh
+++ xray/ins-xray.sh
@@ -709,7 +709,7 @@ cat > /etc/trojan-go/config.json << END
      "reuse_session": true,
       "plain_http_response": "",
        "fallback_addr": "127.0.0.1",
-        "fallback_port": 443,
+        "fallback_port": 2096,
          "fingerprint": "chrome"
     },
     "tcp": {
