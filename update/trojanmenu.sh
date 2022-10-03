5240008366e3e820b8d4dda4fe9cf1d80d7352cc
inoyaksorojawi <71470149+inoyaksorojawi@users.noreply.github.com>
2022-10-03 18:29:33 +0700

    Update trojanmenu.sh

--- update/trojanmenu.sh
+++ update/trojanmenu.sh
@@ -11,25 +11,25 @@ echo -e "02\e[1;33m) Hapus Akun TROJAN WEBSOCKET  \e[m"
 echo -e "03\e[1;33m) Perpanjang Masa Aktif Akun TROJAN WEBSOCKET \e[m"
 echo -e "04\e[1;33m) Cek User Login TROJAN WEBSOCKET \e[m"
 echo -e "\033[1;36m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo -e "05\e[1;33m) Buat Akun TROJAN H2C \e[m"
-echo -e "06\e[1;33m) Hapus Akun TROJAN H2C \e[m"
-echo -e "07\e[1;33m) Perpanjang Masa Aktif Akun TROJAN H2C \e[m"
-echo -e "08\e[1;33m) Cek User Login Trojan H2C \e[m"
+echo -e "05\e[1;33m) Buat Akun TROJAN GRPC \e[m"
+echo -e "06\e[1;33m) Hapus Akun TROJAN GRPC \e[m"
+echo -e "07\e[1;33m) Perpanjang Masa Aktif Akun TROJAN GRPC \e[m"
+echo -e "08\e[1;33m) Cek User Login Trojan GRPC \e[m"
 echo -e "\033[1;36m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo -e "09\e[1;33m) Buat Akun TROJAN GRPC \e[m"
-echo -e "10\e[1;33m) Hapus Akun TROJAN GRPC \e[m"
-echo -e "11\e[1;33m) Perpanjang Masa Aktif Akun TROJAN GRPC \e[m"
-echo -e "12\e[1;33m) Cek User Login TROJAN GRPC \e[m"
+echo -e "09\e[1;33m) Buat Akun TROJAN HTTP \e[m"
+echo -e "10\e[1;33m) Hapus Akun TROJAN HTTP \e[m"
+echo -e "11\e[1;33m) Perpanjang Masa Aktif Akun TROJAN HTTP \e[m"
+echo -e "12\e[1;33m) Cek User Login TROJAN HTTP \e[m"
 echo -e "\033[1;36m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
 echo -e "13\e[1;33m) Buat Akun TROJAN XTLS \e[m"
 echo -e "14\e[1;33m) Hapus Akun TROJAN XTLS  \e[m"
 echo -e "15\e[1;33m) Perpanjang Masa Aktif Akun TROJAN XTLS  \e[m"
 echo -e "16\e[1;33m) Cek User Login TROJAN XTLS  \e[m"
 echo -e "\033[1;36m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo -e "17\e[1;33m) Buat Akun TROJAN HTTP \e[m"
-echo -e "18\e[1;33m) Hapus Akun TROJAN HTTP \e[m"
-echo -e "19\e[1;33m) Perpanjang Masa Aktif Akun TROJAN HTTP \e[m"
-echo -e "20\e[1;33m) Cek User Login TROJAN HTTP \e[m"
+echo -e "17\e[1;33m) Buat Akun TROJAN H2C \e[m"
+echo -e "18\e[1;33m) Hapus Akun TROJAN H2C \e[m"
+echo -e "19\e[1;33m) Perpanjang Masa Aktif Akun TROJAN H2C \e[m"
+echo -e "20\e[1;33m) Cek User Login TROJAN H2C \e[m"
 echo -e "\033[1;36m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
 echo -e "21\e[1;33m) Buat Akun TROJAN GO \e[m"
 echo -e "22\e[1;33m) Hapus Akun TROJAN GO \e[m"
@@ -68,28 +68,28 @@ renewtrojanwss
 cektrojanwss
 ;;
 5)
-addtrojanhdua
-;;
-6)
-deltrojanhdua
-;;
-7)
-renewtrojanhdua
-;;
-8)
-cektrojanhdua
-;;
-9)
 addtrojangrpc
 ;;
-10)
+6)
 deltrojangrpc
 ;;
-11)
+7)
 renewtrojangrpc
 ;;
-12)
+8)
 cektrojangrpc
+;;
+9)
+addtrojanhttp
+;;
+10)
+deltrojanhttp
+;;
+11)
+renewtrojanhttp
+;;
+12)
+cektrojanhttp
 ;;
 13)
 addtrojanxtls
@@ -104,16 +104,16 @@ renewtrojanxtls
 cektrojanxtls
 ;;
 17)
-addtrojanhttp
+addtrojanhdua
 ;;
 18)
-deltrojanhttp
+deltrojanhdua
 ;;
 19)
-renewtrojanhttp
+renewtrojanhdua
 ;;
 20)
-cektrojanhttp
+cektrojanhdua
 ;;
 21)
 addtrgo
