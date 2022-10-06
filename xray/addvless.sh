9acc70322aca32d88f004ba535d8954664d93630
inoyaksorojawi <71470149+inoyaksorojawi@users.noreply.github.com>
2022-10-06 13:05:04 +0700

    Update addvless.sh

--- xray/addvless.sh
+++ xray/addvless.sh
@@ -105,7 +105,7 @@ systemctl restart xss
 service cron restart
 clear
 echo -e "\033[1;31m━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo -e "\033[1;46m 🔰 AKUN VLESS WEBSOCKET 🔰\e[m"   
+echo -e "\033[1;46m 🔥 AKUN VLESS WEBSOCKET 🔥\e[m"   
 echo -e "\033[1;31m━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
 echo -e "Nama➡️ ${user}"
 echo -e "IP➡️ ${MYIP}),$domain2"
@@ -123,7 +123,7 @@ echo -e "WS TLS➡️ ${vlesstls}"
 echo -e "\033[1;31m━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
 echo -e "WS NONTLS➡️ ${vlessnontls}"
 echo -e "\033[1;31m━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo -e "\033[1;46m 🔰LUXURY EDITION ZEROSSL🔰\e[m"   
+echo -e "\033[1;46m 🔥LUXURY EDITION ZEROSSL🔥\e[m"   
 echo -e "\033[1;31m━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
 echo ""
 read -n 1 -s -r -p "Ketik Bebas Untuk Ke Menu Utama"
