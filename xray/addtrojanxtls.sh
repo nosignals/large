d79fcfbe60addd8ba9144a1d77c7367d9b1fb0e5
inoyaksorojawi <71470149+inoyaksorojawi@users.noreply.github.com>
2022-10-06 13:26:57 +0700

    Update addtrojanxtls.sh

--- xray/addtrojanxtls.sh
+++ xray/addtrojanxtls.sh
@@ -87,8 +87,8 @@ sed -i '/#trojan-gfw$/a\### '"$user $exp"'\
 },{"password": "'""$uuid""'","email": "'""$user""'"' /etc/xray/xvless.json
 sed -i '/#trojan-gfw$/a\### '"$user $exp"'\
 },{"password": "'""$uuid""'","email": "'""$user""'"' /etc/xray/xss.json
-trojanxtls="trojan://${uuid}@${domain}:$txtls?security=xtls&type=tcp&headerType=none&flow=xtls-rprx-splice-udp443#%F0%9F%94%B0TROJAN+XTLS+${user}"
-trojangfw="trojan://${uuid}@${domain}:${tgfw}?type=tcp&security=tls&headerType=none#%F0%9F%94%B0TROJAN+GFW+TLS+$user"
+trojanxtls="trojan://${uuid}@${domain}:$txtls?security=xtls&type=tcp&headerType=none&flow=xtls-rprx-splice-udp443#%F0%9F%94%A5TROJAN+XTLS+${user}"
+trojangfw="trojan://${uuid}@${domain}:${tgfw}?type=tcp&security=tls&headerType=none#%F0%9F%94%A5TROJAN+GFW+TLS+$user"
 service cron restart
 systemctl restart xray.service
 systemctl restart xtrojan.service
