3d826b5ffb927f860a3daa61f82e968978104e72
inoyaksorojawi <71470149+inoyaksorojawi@users.noreply.github.com>
2022-10-08 20:32:42 +0700

    Update menutester.sh

--- update/menutester.sh
+++ update/menutester.sh
@@ -32,7 +32,7 @@ echo -e "00\e[1;31m) MENU UTAMA \e[m"
 echo -e "\033[1;36m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
 echo -e "xx\e[1;31m) KELUAR \e[m"
 echo -e "\033[1;31m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo -e "\E[46;1;46m           🔰 LUXURY EDITION BY ZEROSSL 🔰              \e[0m"
+echo -e "\E[46;1;46m            🔰 LUXURY EDITION ZEROSSL 🔰                \e[0m"
 echo -e "\033[1;31m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
 echo -e "\e[1;36m ⇱ Select Number From [ 1 - 0 ] ⇲ \e[m"
 read -p " ⇱ Silahkan Pilih Angka [ 1 - 0 ] ⇲: " menu
