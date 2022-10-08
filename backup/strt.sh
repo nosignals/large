5ab2e9158fd13396d2f7ae4166c7eb1cecaa6836
inoyaksorojawi <71470149+inoyaksorojawi@users.noreply.github.com>
2022-10-08 20:42:25 +0700

    Update strt.sh

--- backup/strt.sh
+++ backup/strt.sh
@@ -14,18 +14,18 @@ LIGHT='\033[0;37m'
 # Getting
 MYIP=$(wget -qO- ipinfo.io/ip);
 clear
-data=( `cat /etc/shadowsocks-libev/akun.conf | grep '^###' | cut -d ' ' -f 2`);
+data=( `cat /etc/shadowsocks-libev/akun.conf | grep '^###' | cut -d ' ' -f2`);
 now=`date +"%Y-%m-%d"`
 for user in "${data[@]}"
 do
-exp=$(grep -w "^### $user" "/etc/shadowsocks-libev/akun.conf" | cut -d ' ' -f 3)
+exp=$(grep -w "^### $user" "/etc/shadowsocks-libev/akun.conf" | cut -d ' ' -f3)
 d1=$(date -d "$exp" +%s)
 d2=$(date -d "$now" +%s)
 exp2=$(( (d1 - d2) / 86400 ))
 printf "$user\n$exp2\n" | addss
 done
 rm -f /etc/shadowsocks-libev/ss.conf
-data=( `cat /etc/wireguard/wg0.conf | grep '^### Client' | cut -d ' ' -f 3`);
+data=( `cat /etc/wireguard/wg0.conf | grep '^### Client' | cut -d ' ' -f3`);
 now=`date +"%Y-%m-%d"`
 for user in "${data[@]}"
 do
