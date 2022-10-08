cf4bbeb9dbb2337ab12f81287616b2738cb84583
inoyaksorojawi <71470149+inoyaksorojawi@users.noreply.github.com>
2022-10-08 20:39:14 +0700

    Update vmessmenu.sh

--- update/vmessmenu.sh
+++ update/vmessmenu.sh
@@ -40,7 +40,7 @@ echo -e "00\e[1;31m) Menu Utama \e[m"
 echo -e "\033[1;36m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
 echo -e "xx\e[1;31m) Keluar \e[m"
 echo -e "\033[1;31m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo -e "\E[43;1;46m            🔰 LUXURY EDITION BY ZEROSSL 🔰             \e[0m"
+echo -e "\E[43;1;46m             🔰 LUXURY EDITION ZEROSSL 🔰               \e[0m"
 echo -e "\033[1;31m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
 read -p "Silahkan Pilih Angka[ 1 - 0 ] : " menu
 echo -e ""
