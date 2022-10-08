0a6849ec132e5ac42409e1e6c284b5ae56dedf07
inoyaksorojawi <71470149+inoyaksorojawi@users.noreply.github.com>
2022-10-08 21:34:22 +0700

    Update cekssh.sh

--- ssh/cekssh.sh
+++ ssh/cekssh.sh
@@ -1,4 +1,22 @@
 #!/bin/bash
+dateFromServer=$(curl -v --insecure --silent https://google.com/ 2>&1 | grep Date | sed -e 's/< Date: //')
+date=`date +"%Y-%m-%d" -d "$dateFromServer"`
+#########################
+
+    data=( `cat /root/tmp | grep -E "^### " | awk '{print $2}'` )
+    for user in "${data[@]}"
+    do
+    exp=( `grep -E "^### $user" "/root/tmp" | awk '{print $3}'` )
+    d1=(`date -d "$exp" +%s`)
+    d2=(`date -d "$biji" +%s`)
+    exp2=$(( (d1 - d2) / 86400 ))
+    if [[ "$exp2" -le "0" ]]; then
+    echo $user > /etc/.$user.ini
+    else
+    rm -f /etc/.$user.ini > /dev/null 2>&1
+    fi
+    done
+    rm -f /root/tmp
 # My Telegram : https://t.me/zerossl
 # ==========================================
 # Color
@@ -15,7 +33,6 @@ LIGHT='\033[0;37m'
 MYIP=$(wget -qO- ipinfo.io/ip);
 clear
 echo " "
-echo " "
 
 if [ -e "/var/log/auth.log" ]; then
         LOG="/var/log/auth.log";
@@ -23,97 +40,88 @@ fi
 if [ -e "/var/log/secure" ]; then
         LOG="/var/log/secure";
 fi
-                
+               
 data=( `ps aux | grep -i dropbear | awk '{print $2}'`);
-lastlogin=$(cat /var/log/auth.log | grep -w "$akun" | tail -n 500 | cut -d " " -f2 | tail -1)
 echo -e "\033[1;31m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo -e "\033[1;46m 🔰 CEK USER SENYAP TAPI GANAS 🔰  \e[m"       
+echo -e "\033[1;46m     🔰 Dropbear User Login 🔰     \E[0m"
 echo -e "\033[1;31m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo "  ID               |  Username         |  IP Address";
-echo -e "user :${LIGHT} ${akun} ${NC}
-${LIGHT}JAM LOGIN : ${lastlogin} WIB ${NC} ";
+echo "ID  |  Username  |  IP Address";
 echo -e "\033[1;31m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo -e "DROPBEAR"
 cat $LOG | grep -i dropbear | grep -i "Password auth succeeded" > /tmp/login-db.txt;
 for PID in "${data[@]}"
-lastlogin=$(cat /var/log/auth.log | grep -w "$akun" | tail -n 500 | cut -d " " -f2 | tail -1)
 do
-cat /tmp/login-db.txt | grep "dropbear\[$PID\]" > /tmp/login-db-pid.txt;
-NUM=`cat /tmp/login-db-pid.txt | wc -l`;
-USER=`cat /tmp/login-db-pid.txt | awk '{print $10}'`;
-IP=`cat /tmp/login-db-pid.txt | awk '{print $12}'`;
-if [ $NUM -eq 1 ]; then
-echo "$PID - $USER - $IP";
-fi
+        cat /tmp/login-db.txt | grep "dropbear\[$PID\]" > /tmp/login-db-pid.txt;
+        NUM=`cat /tmp/login-db-pid.txt | wc -l`;
+        USER=`cat /tmp/login-db-pid.txt | awk '{print $10}'`;
+        IP=`cat /tmp/login-db-pid.txt | awk '{print $12}'`;
+        if [ $NUM -eq 1 ]; then
+                echo "$PID - $USER - $IP";
+        fi
+echo -e "\033[1;31m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+
 done
 echo " "
 echo -e "\033[1;31m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo -e "\033[1;46m 🔰 CEK USER SENYAP TAPI GANAS 🔰  \e[m"       
+echo -e "\033[1;46m      🔰 OpenSSH User Login 🔰     \E[0m"
 echo -e "\033[1;31m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo "  ID               |  Username         |  IP Address";
+echo "ID  |  Username  |  IP Address";
 echo -e "\033[1;31m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-#echo -e "\033[1;31m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo -e "OPENSSH"
 cat $LOG | grep -i sshd | grep -i "Accepted password for" > /tmp/login-db.txt
 data=( `ps aux | grep "\[priv\]" | sort -k 72 | awk '{print $2}'`);
 
 for PID in "${data[@]}"
 do
-cat /tmp/login-db.txt | grep "sshd\[$PID\]" > /tmp/login-db-pid.txt;
-NUM=`cat /tmp/login-db-pid.txt | wc -l`;
-USER=`cat /tmp/login-db-pid.txt | awk '{print $9}'`;
-IP=`cat /tmp/login-db-pid.txt | awk '{print $11}'`;
-if [ $NUM -eq 1 ]; then
-echo "$PID - $USER - $IP";
-fi
+        cat /tmp/login-db.txt | grep "sshd\[$PID\]" > /tmp/login-db-pid.txt;
+        NUM=`cat /tmp/login-db-pid.txt | wc -l`;
+        USER=`cat /tmp/login-db-pid.txt | awk '{print $9}'`;
+        IP=`cat /tmp/login-db-pid.txt | awk '{print $11}'`;
+        if [ $NUM -eq 1 ]; then
+                echo "$PID - $USER - $IP";
+        fi
+echo -e "\033[1;31m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+
 done
 if [ -f "/etc/openvpn/server/openvpn-gandring-tcp.log" ]; then
-echo ""
-echo -e "\033[1;31m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo -e "\033[1;46m 🔰 CEK USER SENYAP TAPI GANAS 🔰  \e[m"       
-echo -e "\033[1;31m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo "  ID               |  Username         |  IP Address";
-echo -e "\033[1;31m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo -e "OPENVPN"
-echo -e "user :${LIGHT} ${akun} ${NC}
-${LIGHT}JAM LOGIN : ${lastlogin} WIB ${NC} ";
-echo -e "\033[1;31m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-cat /etc/openvpn/server/openvpn-gandring-tcp.log | grep -w "^CLIENT_LIST" | cut -d ',' -f 2,3,8 | sed -e 's/,/      /g' > /tmp/vpn-login-tcp.txt
-cat /tmp/vpn-login-tcp.txt
+        echo " "
+        echo -e "\033[1;31m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+        echo -e "\033[1;46m       🔰 OpenVPN TCP User Login 🔰       \E[0m"
+        echo -e "\033[1;31m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+        echo "Username  |  IP Address  |  Connected";
+        echo -e "\033[1;31m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+        cat /etc/openvpn/server/openvpn-gandring-tcp.log | grep -w "^CLIENT_LIST" | cut -d ',' -f 2,3,8 | sed -e 's/,/      /g' > /tmp/vpn-login-tcp.txt
+        cat /tmp/vpn-login-tcp.txt
 fi
-echo -e "\033[1;31m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo -e "\033[1;31m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+
+if [ -f "/etc/openvpn/server/openvpn-gandring-udp.log" ]; then
+        echo " "
+        echo -e "\033[1;31m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+        echo -e "\033[1;46m       🔰 OpenVPN UDP User Login 🔰       \E[0m"
+        echo -e "\033[1;31m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+        echo "Username  |  IP Address  |  Connected";
+        echo -e "\033[1;31m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+        cat /etc/openvpn/server/openvpn-gandring-udp.log | grep -w "^CLIENT_LIST" | cut -d ',' -f 2,3,8 | sed -e 's/,/      /g' > /tmp/vpn-login-udp.txt
+        cat /tmp/vpn-login-udp.txt
+fi
+echo -e "\033[1;31m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
 
 if [ -f "/etc/openvpn/server/openvpn-gandring-ssl-ws.log" ]; then
-echo ""
-echo -e "\033[1;31m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo -e "\033[1;46m 🔰 CEK USER SENYAP TAPI GANAS 🔰  \e[m"       
-echo -e "\033[1;31m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo "  ID               |  Username         |  IP Address";
-echo -e "\033[1;31m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo -e "OPENVPN"
-echo -e "user :${LIGHT} ${akun} ${NC}
-${LIGHT}JAM LOGIN : ${lastlogin} WIB ${NC} ";
-echo -e "\033[1;31m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-cat /etc/openvpn/server/openvpn-gandring-ssl-ws.log | grep -w "^CLIENT_LIST" | cut -d ',' -f 2,3,8 | sed -e 's/,/      /g' > /tmp/vpn-login-tcp.txt
-cat /tmp/vpn-login-gandring-ssl-ws.txt
+        echo " "
+        echo -e "\033[1;31m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+        echo -e "\033[1;46m       🔰 OpenVPN SSL User Login 🔰       \E[0m"
+        echo -e "\033[1;31m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+        echo "Username  |  IP Address  |  Connected";
+        echo -e "\033[1;31m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+        cat /etc/openvpn/server/openvpn-gandring-ssl-ws.log | grep -w "^CLIENT_LIST" | cut -d ',' -f 2,3,8 | sed -e 's/,/      /g' > /tmp/vpn-login-udp.txt
+        cat /tmp/vpn-login-udp.txt
 fi
-echo -e "\033[1;31m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-if [ -f "/etc/openvpn/server/openvpn-gandring-udp.log" ]; then
-echo " "
-echo -e "\033[1;31m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo -e "\033[1;46m 🔰 CEK USER SENYAP TAPI GANAS 🔰  \e[m"       
-echo -e "\033[1;31m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo "  ID               |  Username         |  IP Address";
-echo -e "user :${LIGHT} ${akun} ${NC}
-${LIGHT}JAM LOGIN : ${lastlogin} WIB ${NC} ";
-echo -e "\033[1;31m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-cat /etc/openvpn/server/openvpn-gandring-udp.log | grep -w "^CLIENT_LIST" | cut -d ',' -f 2,3,8 | sed -e 's/,/      /g' > /tmp/vpn-login-udp.txt
-cat /tmp/vpn-login-gandring-udp.txt
-fi
-echo -e "\033[1;31m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo -e "\033[1;46m    🔰SOLO THE SPIRIT OF JAVA🔰    \e[m"   
-echo -e "\033[1;31m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo -e "\033[1;31m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo "";
 
-echo ""
-read -n 1 -s -r -p "Ketik Bebas Untuk Ke Menu Utama"
-menu
+rm -f /tmp/login-db-pid.txt
+rm -f /tmp/login-db.txt
+rm -f /tmp/vpn-login-tcp.txt
+rm -f /tmp/vpn-login-udp.txt
+echo -e""
+read -n 1 -s -r -p "Tekan Bebas Untuk Ke Menu"
+sshovpnmenu
