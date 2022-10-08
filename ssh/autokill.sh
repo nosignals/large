3a6de61c952efb464e7416ba67544302cd2dd5cc
inoyaksorojawi <71470149+inoyaksorojawi@users.noreply.github.com>
2022-10-08 20:58:56 +0700

    Update autokill.sh

--- ssh/autokill.sh
+++ ssh/autokill.sh
@@ -17,7 +17,7 @@ clear
 Green_font_prefix="\033[32m" && Red_font_prefix="\033[31m" && Green_background_prefix="\033[42;37m" && Red_background_prefix="\033[41;37m" && Font_color_suffix="\033[0m"
 Info="${Green_font_prefix}[ON]${Font_color_suffix}"
 Error="${Red_font_prefix}[OFF]${Font_color_suffix}"
-cek=$(grep -c -E "^# Autokill" /etc/cron.d/tendang)
+cek=$(grep -c -E "^# Autokill" /etc/cron.d/gajol)
 if [[ "$cek" = "1" ]]; then
 sts="${Info}"
 else
@@ -28,11 +28,11 @@ echo -e ""
 echo -e "=================================="
 echo -e "       Status Autokill $sts       "
 echo -e "=================================="
-echo -e "1. AutoKill After 5 Minutes"
-echo -e "2. AutoKill After 10 Minutes"
-echo -e "3. AutoKill After 15 Minutes"
-echo -e "4. Turn Off AutoKill/MultiLogin"
-echo -e "5. Exit"
+echo -e "1. AutoKill Setiap 5 Minutes"
+echo -e "2. AutoKill Setiap 10 Minutes"
+echo -e "3. AutoKill Setiap 15 Minutes"
+echo -e "4. Matikan AutoKill/MultiLogin"
+echo -e "5. Keluar"
 echo -e "=================================="                                                                                                          
 echo -e ""
 read -p "Select From Options [1-4 or x] :  " AutoKill
@@ -43,14 +43,14 @@ case $AutoKill in
                 echo -e ""
                 sleep 1
                 clear
-                echo > /etc/cron.d/tendang
-                echo "# Autokill" >>/etc/cron.d/tendang
-                echo "*/5 * * * *  root /usr/bin/tendang $max" >>/etc/cron.d/tendang
+                echo > /etc/cron.d/gajol
+                echo "# Autokill" >>/etc/cron.d/gajol
+                echo "*/5 * * * *  root /usr/bin/gajol $max" >>/etc/cron.d/gajol
                 echo -e ""
                 echo -e "======================================"
                 echo -e ""
-                echo -e "      Allowed MultiLogin : $max"
-                echo -e "      AutoKill Every     : 5 Minutes"      
+                echo -e "      Batas MultiLogin   : $max"
+                echo -e "      AutoKill Setiap    : 5 Minutes"      
                 echo -e ""
                 echo -e "======================================"                                                                                                                                 
                 exit                                                                  
@@ -59,14 +59,14 @@ case $AutoKill in
                 echo -e ""
                 sleep 1
                 clear
-                echo > /etc/cron.d/tendang
-                echo "# Autokill" >>/etc/cron.d/tendang
-                echo "*/10 * * * *  root /usr/bin/tendang $max" >>/etc/cron.d/tendang
+                echo > /etc/cron.d/gajol
+                echo "# Autokill" >>/etc/cron.d/gajol
+                echo "*/10 * * * *  root /usr/bin/gajol $max" >>/etc/cron.d/gajol
                 echo -e ""
                 echo -e "======================================"
                 echo -e ""
-                echo -e "      Allowed MultiLogin : $max"
-                echo -e "      AutoKill Every     : 10 Minutes"
+                echo -e "      Batas MultiLogin    : $max"
+                echo -e "      AutoKill Setiap     : 10 Minutes"
                 echo -e ""
                 echo -e "======================================"
                 exit
@@ -75,21 +75,21 @@ case $AutoKill in
                 echo -e ""
                 sleep 1
                 clear
-                echo > /etc/cron.d/tendang
-                echo "# Autokill" >>/etc/cron.d/tendang
-                echo "*/15 * * * *  root /usr/bin/tendang $max" >>/etc/cron.d/tendang
+                echo > /etc/cron.d/gajol
+                echo "# Autokill" >>/etc/cron.d/gajol
+                echo "*/15 * * * *  root /usr/bin/gajol $max" >>/etc/cron.d/gajol
                 echo -e ""
                 echo -e "======================================"
                 echo -e ""
-                echo -e "      Allowed MultiLogin : $max"
-                echo -e "      AutoKill Every     : 15 Minutes"
+                echo -e "      Batas MultiLogin :   $max"
+                echo -e "      AutoKill Setiap     : 15 Minutes"
                 echo -e ""
                 echo -e "======================================"
                 exit
                 ;;
                 4)
                 clear
-                echo > /etc/cron.d/tendang
+                echo > /etc/cron.d/gajol
                 echo -e ""
                 echo -e "======================================"
                 echo -e ""
