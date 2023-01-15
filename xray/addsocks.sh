2239d7df569c28a2b6f1119ff307b02f82a3f52b
inoyaksorojawi <71470149+inoyaksorojawi@users.noreply.github.com>
2023-01-15 13:12:11 +0700

    Update addsocks.sh

--- xray/addsocks.sh
+++ xray/addsocks.sh
@@ -1,1 +1,134 @@
-1
+#!/bin/bash
+# My Telegram : https://t.me/zerossl
+# ==========================================
+# Color
+RED='\033[0;31m'
+NC='\033[0m'
+GREEN='\033[0;32m'
+ORANGE='\033[0;33m'
+BLUE='\033[0;34m'
+PURPLE='\033[0;35m'
+CYAN='\033[0;36m'
+LIGHT='\033[0;37m'
+# ==========================================
+# Getting
+MYIP=$(wget -qO- ipinfo.io/ip);
+clear
+domain=$(cat /etc/xray/domain)
+domain2=$(cat /etc/xray/domain2)
+domain3=$(cat /etc/xray/domain3)
+dateFromServer=$(curl -v --insecure --silent https://google.com/ 2>&1 | grep Date | sed -e 's/< Date: //')
+date=`date +"%Y-%m-%d" -d "$dateFromServer"`
+#uuid=$(cat /proc/sys/kernel/random/uuid)
+ttls="$(cat ~/log-install.txt | grep -w "SS 2022 WS TLS" | cut -d: -f2|sed 's/ //g')"
+tnontls="$(cat ~/log-install.txt | grep -w "SS 2022 WS NON TLS" | cut -d: -f2|sed 's/ //g')"
+until [[ $user =~ ^[a-zA-Z0-9_]+$ && ${user_EXISTS} == '0' ]]; do
+read -rp "Username : " -e user
+read -rp "Password : " -e pass
+user_EXISTS=$(grep -w $user /etc/xray/xtrojan.json | wc -l)
+
+if [[ ${user_EXISTS} == '1' ]]; then
+echo ""
+echo -e "Username ${RED}${user}${NC} Already On VPS Please Choose Another"
+exit 1
+fi
+done
+
+until [[ $user =~ ^[a-zA-Z0-9_]+$ && ${user_EXISTS} == '0' ]]; do
+read -rp "Username : " -e user
+read -rp "Password : " -e pass
+user_EXISTS=$(grep -w $user /etc/xray/xvmess.json | wc -l)
+
+if [[ ${user_EXISTS} == '1' ]]; then
+echo ""
+echo -e "Username ${RED}${user}${NC} Already On VPS Please Choose Another"
+exit 1
+fi
+done
+
+until [[ $user =~ ^[a-zA-Z0-9_]+$ && ${user_EXISTS} == '0' ]]; do
+read -rp "Username : " -e user
+read -rp "Password : " -e pass
+user_EXISTS=$(grep -w $user /etc/xray/config.json | wc -l)
+
+if [[ ${user_EXISTS} == '1' ]]; then
+echo ""
+echo -e "Username ${RED}${user}${NC} Already On VPS Please Choose Another"
+exit 1
+fi
+done
+
+until [[ $user =~ ^[a-zA-Z0-9_]+$ && ${user_EXISTS} == '0' ]]; do
+read -rp "Username : " -e user
+read -rp "Password : " -e pass
+user_EXISTS=$(grep -w $user /etc/xray/xvless.json | wc -l)
+
+if [[ ${user_EXISTS} == '1' ]]; then
+echo ""
+echo -e "Username ${RED}${user}${NC} Already On VPS Please Choose Another"
+exit 1
+fi
+done
+
+until [[ $user =~ ^[a-zA-Z0-9_]+$ && ${user_EXISTS} == '0' ]]; do
+read -rp "Username : " -e user
+read -rp "Password : " -e pass
+user_EXISTS=$(grep -w $user /etc/xray/xss.json | wc -l)
+
+if [[ ${user_EXISTS} == '1' ]]; then
+echo ""
+echo -e "Username ${RED}${user}${NC} Already On VPS Please Choose Another"
+exit 1
+fi
+done
+base64=$(openssl rand -base64 16)
+uuid=$(openssl rand -hex 7)
+read -p "Expired (Days) : " masaaktif
+hariini=`date -d "0 days" +"%Y-%m-%d"`
+exp=`date -d "$masaaktif days" +"%Y-%m-%d"`
+sed -i '/#ss-tls$/a\### '"$user $exp"'\
+},{"password": "'""GANdRinGcq34MSCDTOck0g==:$base64""'","email": "'""$user""'"' /etc/xray/xvmess.json
+sed -i '/#ss-tls$/a\### '"$user $exp"'\
+},{"password": "'""GANdRinGcq34MSCDTOck0g==:$base64""'","email": "'""$user""'"' /etc/xray/xvless.json
+sed -i '/#ss-tls$/a\### '"$user $exp"'\
+},{"password": "'""GANdRinGcq34MSCDTOck0g==:$base64""'","email": "'""$user""'"' /etc/xray/xtrojan.json
+sed -i '/#ss-nontls$/a\### '"$user $exp"'\
+},{"password": "'""GANdRinGcq34MSCDTOck0g==:$base64""'","email": "'""$user""'"' /etc/xray/xtrojan.json
+sed -i '/#ss-nontls$/a\### '"$user $exp"'\
+},{"password": "'""GANdRinGcq34MSCDTOck0g==:$base64""'","email": "'""$user""'"' /etc/xray/config.json
+
+socks5tls="socks://GANdRinGcq34MSCDTOck0g==:$base64@${domain}:$ttls?type=ws&security=tls&host=$domain3&path=%2fGANDRING-WS&sni=$domain3#%F0%9F%94%A5TROJAN+WS+TLS+${user}"
+socks5nontls="socks://GANdRinGcq34MSCDTOck0g==:$base64@${domain}:$tnontls?type=ws&security=none&host=$domain3&path=%2fGANDRING-WS#%F0%9F%94%A5TROJAN+WS+NONTLS+${user}"
+systemctl restart xvmess
+systemctl restart xray.service
+systemctl restart xtrojan.service
+systemctl restart xvless
+systemctl restart xss
+#systemctl restart nginx
+service cron restart
+clear
+echo -e ""
+echo -e "\033[1;31m━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo -e "\033[1;46m🔥AKUN SOCKS5 WEBSOCKET 🔥\e[m"
+echo -e "\033[1;31m━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo -e "Nama➡️ ${user}"
+echo -e "IP➡️ ${MYIP},$domain2"
+echo -e "Host➡️ ${domain}"
+echo -e "CF Host➡️ ${domain3}"
+echo -e "Protocol➡️ websocket"
+echo -e "Path➡️ /GANDRING-WS ,/worryfree/ ,/kuota-habis/"
+echo -e "TLS➡️ ${ttls},8443,2096,2087,2083,2053"
+echo -e "NONTLS➡️ ${tnontls},2095,2086,2082,2052"
+echo -e "Sandi➡️ GANdRinGcq34MSCDTOck0g==:$base64"
+echo -e "Dibuat➡️ $hariini"
+echo -e "Kadaluarsa➡️ $exp"
+echo -e "\033[1;31m━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo -e "WS TLS➡️ ${socks5tls}"
+echo -e "\033[1;31m━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo -e "WS NONTLS➡️ ${socks5nontls}"
+echo -e "\033[1;31m━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo -e "\033[1;46m🔥LUXURY EDITION ZEROSSL🔥\e[m"
+echo -e "\033[1;31m━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo ""
+read -n 1 -s -r -p "Ketik Bebas Untuk Ke Menu Utama"
+ssmenu
